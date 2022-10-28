99a79e3765f03f8b947b0817d092fb9c0655bfff
L0veR1ck <lavr.rar@yandex.ru>
2022-10-28 18:09:31 +0500

    дописал все тестовые функции

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,58 +5,36 @@ namespace MyStringBuilderTask
 {
     public class MyStringBuilderTests
     {
-        public static void TestAppend(string oldValue, string expectedResult)
+        public static void TestAppendString(string oldValue, string toAppend, string expectedResult)
         {
             var actualResult = new MyStringBuilder();
-            SetOldValue(actualResult, oldValue);
-            
-            Assert.AreEqual(expectedResult, actualResult.ToString());
+            actualResult.Value = GetListOfChars(oldValue);
+            actualResult.Append(toAppend);
+            Assert.AreEqual(GetListOfChars(expectedResult), actualResult.Value);
         }
 
         public static void TestClear(string oldValue)
         {
             var actualResult = new MyStringBuilder();
-            SetOldValue(actualResult, oldValue);
+            actualResult.Value = GetListOfChars(oldValue);
             actualResult.Clear();
             Assert.AreEqual(new List<char>(), actualResult.Value);
         }
 
-        private static void SetOldValue(MyStringBuilder sb, string oldValue)
+        public static void TestToString(string oldValue, string expectedResult)
         {
-            var oldValueList = new List<char>();
-            foreach (var cur in oldValue)
-            {
-                oldValueList.Add(cur);
-            }
-
-            sb.Value = oldValueList;
+            var actualResult = new MyStringBuilder();
+            actualResult.Value = GetListOfChars(oldValue);
+            Assert.AreEqual(expectedResult, actualResult.ToString());
         }
 
-        private static void IsEqualToString(MyStringBuilder sb, string expected)
+        private static List<char> GetListOfChars(string oldString)
         {
-            
-        }
+            var newList = new List<char>();
+            foreach (var cur in oldString)
+                newList.Add(cur);
 
-        public static void TestToString()
-
-        [TestCase(@"", new string[0])]
-        [TestCase("text", new[] { "text" })]
-        [TestCase("hello world", new[] { "hello", "world" })]
-        [TestCase(@"kek    lol", new[] { @"kek", @"lol" })]
-        [TestCase(@"""'kek'""", new[] { @"'kek'" })]
-        [TestCase(@"'""kek""'", new[] { @"""kek""" })]
-        [TestCase(@"""""", new[] { @"" })]
-        [TestCase(@"'   '", new[] { @"   " })]
-        [TestCase(@"""kek", new[] { @"kek" })]
-        [TestCase(@"kek""\lol""prikol", new[] { @"kek", @"lol", @"prikol" })]
-        [TestCase(@"""\\""", new[] { @"\" })]
-        [TestCase(@"'\'\''", new[] { @"''" })]
-        [TestCase(@"""\""""", new[] { @"""" })]
-        [TestCase(@"  a", new[] { @"a" })]
-        [TestCase(@"'kek ", new[] { @"kek " })]
-        public static void RunTests(string input, string[] expectedOutput)
-        {
-            Test(input, expectedOutput);
+            return newList;
         }
     }
 }
