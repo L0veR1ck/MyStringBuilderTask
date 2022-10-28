3bcd07e6a321695488dc24e7c1457fbba062cde3
Карамов Артур <32008462+karamov-git@users.noreply.github.com>
2022-10-28 14:22:40 +0500

    Add files via upload

---
+++ MyStringBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace MyStringBuilderTask
+{
+	//Необходимо создать свою реализацию StringBuilder. 
+	//Нужно уметь добавлять строку/символ, чистить билдер и преваращать в свтроку
+	//В классе MyStringBuilder реализуйте необходимый функционал 
+	//В классе MyStringBuilderTests продеманстрируйте использоване вашей реализации 
+	public class MyStringBuilder
+	{
+		
+	}
+}	
---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MyStringBuilderTask
+{
+	public class MyStringBuilderTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
